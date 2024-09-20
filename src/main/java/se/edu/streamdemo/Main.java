ef03347b91604fa605856077b94a9eb0632678ad
g13nd0n-admin <e0957797@u.nus.edu>
2024-09-20 17:37:03 +0800

    Use lambda to filter data and sort

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -3,8 +3,10 @@ package se.edu.streamdemo;
 import se.edu.streamdemo.data.Datamanager;
 import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
-
+import se.edu.streamdemo.task.TaskComparator;
 import java.util.ArrayList;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,13 +15,16 @@ public class Main {
         ArrayList<Task> tasksData = dataManager.loadData();
         // C:\Users\glend\OneDrive\Documents\Tasks\data
         // dont use absolute path as it may not exist when using application on a different computer
-        System.out.println("Printing all data ...");
-        printAllData(tasksData);
+        //System.out.println("Printing all data ...");
+        //printAllData(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlinesUsingStream(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
+        printAllData(filteredList);
 
     }
 
@@ -45,6 +50,21 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing deadlines using stream");
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((t1, t2) -> t1.getDescription().compareTo(t2.getDescription()))
+                .forEach(t -> System.out.println(t));
+    }
+
+    public static ArrayList<Task> filterTasksByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredList;
     }
 
 }
